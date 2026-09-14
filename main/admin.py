efb204fe9bbcd18cce244c9720fe7fa4b0b31480
melin09-daz <amelindafedora09@gmail.com>
2026-09-14 16:08:40 +0700

    feat: akses admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from main.models import Experience, Project
-
+from .models import Experience, Project
 # Register your models here.
-admin.site.register(Experience)
-admin.site.register(Project)
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'started_at', 'ended_at')
+    list_filter = ('category',)
+    search_fields = ('title', 'organization', 'description')
+    
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'category', 'tech_stack')
+    list_filter = ('category',)
+    
+    
