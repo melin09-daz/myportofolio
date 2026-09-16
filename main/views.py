d1fb8ee35c368da49b4c85cb1988cbd7e866792c
melin09-daz <amelindafedora09@gmail.com>
2026-09-16 17:36:56 +0700

    feat: tutorial 03

--- main/views.py
+++ main/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.shortcuts import render
 from django.db.utils import OperationalError, ProgrammingError
-from main.models import Experience, Project, ProjectForm
+from main.models import Experience, Project
+from main.forms import ProjectForm
 
 def show_main(request):
     context = {
