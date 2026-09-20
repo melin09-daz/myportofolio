69bcf281e59b066b60d93222426bbf2265678680
melin09-daz <amelindafedora09@gmail.com>
2026-09-20 10:19:29 +0700

    feat: link/url untuk akses experience add dan delete

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_project, create_project, get_projects_json, delete_project
+from main.views import show_main, show_experience, create_experience, delete_experience, show_project, create_project, get_projects_json, delete_project
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("experiences/add/", create_experience, name="create_experience"),
+    path("experiences/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
     path("project/", show_project, name="show_project"),
     path("projects/add/", create_project, name="create_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
