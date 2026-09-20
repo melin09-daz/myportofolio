50d7a57026cea7197be8ce0807ae97d855e6dbe1
melin09-daz <amelindafedora09@gmail.com>
2026-09-20 09:36:04 +0700

    feat: form experience

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,78 @@
 from django.forms import ModelForm, TextInput, Textarea, URLInput
 
-from main.models import Project
+from main.models import Experience, Project
+
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = Experience
+        fields = [
+            "title",
+            "description",
+            "category",
+            "start_month",
+            "start_year",
+            "end_month",
+            "end_year",
+            "is_ongoing",
+            "logo",
+        ]
+
+        labels = {
+            "title": "Judul Pengalaman",
+            "description": "Deskripsi Pengalaman",
+            "category": "Kategori Pengalaman",
+            "start_month": "Bulan Mulai",
+            "start_year": "Tahun Mulai",
+            "end_month": "Bulan Akhir",
+            "end_year": "Tahun Akhir",
+            "is_ongoing": "Masih Berlangsung",
+            "logo": "Logo",
+        }
+
+        widgets = {
+            "title": TextInput(
+                attrs={
+                    "placeholder": "Nama Pengalaman",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Ceritakan Pengalamanmu",
+                    "rows": 3,
+                }
+            ),
+            "category": TextInput(
+                attrs={
+                    "placeholder": "part-time",
+                }
+            ),
+            "start_month": TextInput(
+                attrs={
+                    "placeholder": "1-12",
+                }
+            ),
+            "start_year": TextInput(
+                attrs={
+                    "placeholder": "2025",
+                }
+            ),
+            "end_month": TextInput(
+                attrs={
+                    "placeholder": "1-12",
+                }
+            ),
+            "end_year": TextInput(
+                attrs={
+                    "placeholder": "2025",
+                }
+            ),
+            "logo": URLInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
+                }
+            ),
+        }
 
 class ProjectForm(ModelForm):
     class Meta:
